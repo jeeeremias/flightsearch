2f8dcf52113c3f0cbe915e983e7dc129978ad063
Jeremias Reis <jeremias.reis@symphony.com>
2019-02-19 15:53:44 -0300

    Created API documentation

--- src/main/java/com/flightsearch/flight/FlightController.java
+++ src/main/java/com/flightsearch/flight/FlightController.java
@@ -5,9 +5,11 @@ import com.flightsearch.flight.model.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,17 +22,26 @@ public class FlightController {
 
     private final FlightService flightService;
     private final DateFormat dateFormat;
+    private final DateFormat requestDateFormat;
 
     @Autowired
     public FlightController(FlightService flightService) {
         this.flightService = flightService;
         this.dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+        this.requestDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     }
 
     @GetMapping(value = "/flights", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<ResponseDTO> getFlights(String airportFrom, String airportTo, long date) {
-        List<Flight> directFlights = flightService.getDirectFlights(airportFrom, airportTo, new Date(date));
-        List<LinkedList<Flight>> indirectFlights = flightService.getIndirectFlights(airportFrom, airportTo, new Date(date));
+    public List<ResponseDTO> getFlights(@RequestParam String airportFrom,
+        @RequestParam String airportTo, @RequestParam String date)
+        throws ParseException {
+        Date dateFlight = requestDateFormat.parse(date);
+        List<Flight> directFlights = flightService.getDirectFlights(airportFrom,
+            airportTo,dateFlight);
+
+        List<LinkedList<Flight>> indirectFlights = flightService.getIndirectFlights(airportFrom,
+            airportTo, dateFlight);
+
         List<ResponseDTO> response = new ArrayList<>();
         for (Flight directFlight : directFlights) {
             response.add(new ResponseDTO(directFlight, dateFormat));
